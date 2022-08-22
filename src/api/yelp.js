c894e9251a0edf4e7f5bf68ed9750ff00a57d111
Mert KELLECİ <44211570+MertKelleci@users.noreply.github.com>
2022-08-22 19:19:02 +0300

    Api Key Removed

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer 48y-vbx5oSjEfyerUBC29gX2MbPS-QbAD3R12H2Mb0rBP1k9LHgOEA41jf0dGCiz01_gQaZW0aO5oGPpXUADXdluukdwLZbsu0TgcdooeqHdj5DIqw8_hdcavt-DYXYx",
+      <YOUR_API_KEY>,
   },
 });
